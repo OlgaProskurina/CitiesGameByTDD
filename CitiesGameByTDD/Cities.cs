ac278ff6b6dd936aa1298d635113a27d685568dc
OlgaProskurina <lich@DESKTOP-KABUT51>
2023-03-17 11:42:41 +0700

    LoadCities method trivial implementation

---
+++ CitiesGameByTDD/Cities.cs
@@ -0,0 +1,16 @@
+﻿namespace CitiesGameByTDD
+{
+    public class Cities
+    {
+        private List<City> _cities;
+
+        //TODO: дореализовать загрузку списка
+        public bool LoadCities()
+        {
+            _cities = new List<City>();
+            if (_cities.Count == 0)
+                return false;
+            return true;
+        }
+    }
+}
---
+++ CitiesGameByTDDTests/CitiesTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CitiesGameByTDD.Tests
+{
+    [TestClass()]
+    public class CitiesTests
+    {
+        [TestMethod()]
+        public void LoadCitiesTest()
+        {
+            var cities = new Cities();           
+            Assert.IsFalse(cities.LoadCities());
+        }
+    }
+}
