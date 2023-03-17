5de435310ece3e736a8c89cac385011c195519dd
OlgaProskurina <lich@DESKTOP-KABUT51>
2023-03-17 11:36:58 +0700

    GetCitiesFromBD method trivial implementation

---
+++ CitiesGameByTDD/CitiesGameSQLiteLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SQLite;
+
+namespace CitiesGameByTDD
+{
+    public class CitiesGameSQLiteLoader
+    {
+       // TODO: реализовать загрузку городов из БД 
+       public List<City> GetCitiesFormBD()
+       {
+           return new List<City>();
+       }        
+
+    }
+}
---
+++ CitiesGameByTDDTests/CitiesGameSQLiteLoaderTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CitiesGameByTDD.Tests
+{
+    [TestClass()]
+    public class CitiesGameSQLiteLoaderTests
+    {
+        [TestMethod()]
+        public void GetCitiesFormBDEmptyListTest()
+        {
+            var loader = new CitiesGameSQLiteLoader();            
+            Assert.AreEqual(0, loader.GetCitiesFormBD().Count);
+        }
+    }
+}
