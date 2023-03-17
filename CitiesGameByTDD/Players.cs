f3cae9a26df437750e7ce174e2f5938243cc13ca
OlgaProskurina <lich@DESKTOP-KABUT51>
2023-03-17 10:42:24 +0700

    Players set method added

---
+++ CitiesGameByTDD/Players.cs
@@ -0,0 +1,19 @@
+﻿namespace CitiesGameByTDD
+{
+    public class Players
+    {
+        private List<int> _playersList = new List<int>();
+        public IReadOnlyList<int> PlayersList => _playersList;
+
+
+        public void SetPlayers(int playersCount)
+        {
+            for (int i = 1; i < playersCount + 1; i++)
+            {
+                _playersList.Add(i);
+            }            
+        }
+
+        
+    }
+}
---
+++ CitiesGameByTDDTests/PlayersTests.cs
@@ -0,0 +1,27 @@
+﻿using CitiesGameByTDD;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CitiesGameByTDD.Tests
+{
+    [TestClass()]
+    public class PlayersTests
+    {
+        private Players _players;
+
+        [TestInitialize]
+        public void Init() 
+        {
+            _players = new Players();
+        }
+
+
+        [TestMethod()]
+        public void SetPlayersTest()
+        {
+            _players.SetPlayers(5);
+            Assert.AreEqual(5, _players.PlayersList.Count);
+        }
+
+        
+    }
+}
