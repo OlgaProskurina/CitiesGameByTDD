5db9968fe5d1eccd0b40b2f4bed348c117a790f3
OlgaProskurina <lich@DESKTOP-KABUT51>
2023-03-17 11:16:04 +0700

    City creation method

---
+++ CitiesGameByTDD/City.cs
@@ -0,0 +1,12 @@
+﻿namespace CitiesGameByTDD
+{
+    public class City
+    {
+        public string Name { get; init; }
+
+        public City(string cityName) 
+        {
+            Name = cityName.Trim().ToLowerInvariant();
+        }
+    }
+}
---
+++ CitiesGameByTDDTests/CityTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CitiesGameByTDD.Tests
+{
+    [TestClass()]
+    public class CityTests
+    {
+        [TestMethod()]
+        public void CityTest()
+        {
+            var cityName = "астана";
+            var city = new City(cityName);
+            Assert.AreEqual(cityName, city.Name);
+        }
+    }
+}
