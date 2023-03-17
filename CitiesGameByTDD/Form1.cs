c91b599324b0556a224f1475cd20936de14490e8
OlgaProskurina <lich@DESKTOP-KABUT51>
2023-03-17 21:22:45 +0700

    Move logic added

--- CitiesGameByTDD/Form1.cs
+++ CitiesGameByTDD/Form1.cs
@@ -4,11 +4,27 @@ namespace CitiesGameByTDD
     {
         private Players _players;
         private Cities _cities;
-        private int moveTimeLeft = 180;
+        private int moveTime = 180;
+        private int moveTimeLeft;
 
         public Form1()
-        {            
+        {
+            _players = new Players();
+            _cities = new Cities();
+            _players.SetPlayers(4);
+            _cities.LoadCities();
+            _cities.FillLetterCountersKeys();
+            _cities.FillLetterCountersValues();
             InitializeComponent();
+        }
+
+        private void GameMove()
+        {
+            labelMessage.Text = String.Empty;
+            moveTimeLeft = moveTime;
+            labelPlayer.Text = _players.CurrentPlayer.ToString();
+            labelCurrentLetter.Text = _cities.CurrentLetter.ToString();
+            timerMove.Start();
         }
 
         private void buttonSurrender_Click(object sender, EventArgs e)
@@ -29,6 +45,7 @@ namespace CitiesGameByTDD
             {
                 labelMessage.Text = $"Игрок номер {_players.CurrentPlayer} выбывает!";
                 _players.ExpelCurrentPlayer();
+                _players.NextPlayer();
                 timerMove.Stop();
             }
 
